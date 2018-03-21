b81c6917dbb4ac8e5b23b95a0228c63b0f556ee2
Aldo Mercado <amercado1014@gmail>
2018-03-21 15:32:58 -0600

    Add people data

--- src/cleaner.js
+++ src/cleaner.js
@@ -7,4 +7,25 @@ const cleanMovieData = (movieData) => {
   };
 };
 
-export { cleanMovieData };
+const cleanPeopleData = (peopleDataArray) => {
+  const promises = peopleDataArray.map(async person => {
+    const { name, homeworld, species } = person;
+
+    const homeworldResponse = await fetch(homeworld);
+    const homeworldData = await homeworldResponse.json();
+
+    const speciesResponse = await fetch(species);
+    const speciesData = await speciesResponse.json();
+
+    return {
+      name: name,
+      homeworld: homeworldData.name,
+      species: speciesData.name,
+      population: homeworldData.population,
+      language: speciesData.language
+    };
+  });
+  return Promise.all(promises);
+};
+
+export { cleanMovieData, cleanPeopleData };
