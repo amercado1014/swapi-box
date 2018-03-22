89e8b1b10a92486ac3766faa68a487e8d359fdc1
Aldo Mercado <amercado1014@gmail>
2018-03-21 21:03:33 -0600

    Add display data with button functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,8 @@ import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
 import CardContainer from '../CardContainer/CardContainer';
 import Favorites from '../Favorites/Favorites';
-import Card from '../Card/Card';
 import Summary from '../Summary/Summary';
-import { 
-  getMovieData, 
-  getPeopleData, 
-  getPlanetData, 
-  getVehicleData 
-} from '../../api';
+import Swapi from '../../api';
 import './App.css';
 
 class App extends Component {
@@ -19,32 +13,45 @@ class App extends Component {
     this.state = {
       favorites: [],
       film: {},
+      cards: [],
       people: [],
       planets: [],
-      vehicles: []
+      vehicles: [],
+      errorStatus: false
     };
   }
 
   async componentDidMount() {
-    const film = await getMovieData();
-    const people = await getPeopleData();
-    const planets = await getPlanetData();
-    const vehicles = await getVehicleData();
-    this.setState({film, people, planets, vehicles});
+    await this.fetchApiData();
+  }
+  
+  fetchApiData = async () => {
+    try {
+      const film = await Swapi.getMovieData();
+      const people = await Swapi.getPeopleData();
+      const planets = await Swapi.getPlanetData();
+      const vehicles = await Swapi.getVehicleData();
+      this.setState({film, people, planets, vehicles});
+    } catch (error) {
+      this.setState({ errorStatus: true });
+    }  
+  }
+
+  setCardsArray = (type) => {
+    this.setState({cards: this.state[type]});
   }
 
   render() {
-    const { film } = this.state;
+    const { favorites, film, cards } = this.state;
     return (
       <div className="App"> 
         <h1 className="title">SWAPI-Box</h1>
         <div className="flex">
           <Summary film={film}/>
           <div className="right-side">
-            <Nav />
+            <Nav setCards={this.setCardsArray} />
             <Favorites />
-            <CardContainer />
-            <Card />
+            <CardContainer cards={cards} />
           </div>
         </div>
       </div>
--- src/api.js
+++ src/api.js
@@ -8,47 +8,31 @@ import {
 const root = 'https://swapi.co/api/';
 
 const getMovieData = async () => {
-  try {
-    const ramdomNumber = Math.floor((Math.random() * 7) + 1);
-    const response = await fetch(`${root}films/${ramdomNumber}`);
-    const data =  await response.json();
-    return cleanMovieData(data);
-  } catch (error) {
-    console.log('Error: ', error);
-  }
+  const ramdomNumber = Math.floor((Math.random() * 7) + 1);
+  const response = await fetch(`${root}films/${ramdomNumber}`);
+  const data =  await response.json();
+  return cleanMovieData(data);
 };
 
 const getPeopleData = async () => {
-  try {
-    const response = await fetch(`${root}people`);
-    const data = await response.json();
-    return cleanPeopleData(data.results);
-  } catch (error) {
-    console.log('Error: ', error);
-  }
+  const response = await fetch(`${root}people`);
+  const data = await response.json();
+  return cleanPeopleData(data.results);
 };
 
 const getPlanetData = async () => {
-  try {
-    const response = await fetch(`${root}planets`);
-    const data = await response.json();
-    return cleanPlanetData(data.results);
-  } catch (error) {
-    console.log('Error: ', error);
-  }
+  const response = await fetch(`${root}planets`);
+  const data = await response.json();
+  return cleanPlanetData(data.results);
 };
 
 const getVehicleData = async () => {
-  try {
-    const response = await fetch(`${root}vehicles`);
-    const data = await response.json();
-    return cleanVehicleData(data.results);
-  } catch (error) {
-    console.log('Error: ', error);
-  }
+  const response = await fetch(`${root}vehicles`);
+  const data = await response.json();
+  return cleanVehicleData(data.results);
 };
 
-export { 
+export default { 
   getMovieData, 
   getPeopleData, 
   getPlanetData, 
