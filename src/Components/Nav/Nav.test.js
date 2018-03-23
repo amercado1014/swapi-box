60361c3808fbf183e9915620efe2c63d273be1c4
Aldo Mercado <amercado1014@gmail>
2018-03-23 15:33:50 -0600

    Add nav tests

--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -2,14 +2,42 @@ import React from 'react';
 import Nav from './Nav';
 import { shallow } from 'enzyme';
 
-describe('Card', () => {
+describe('Nav', () => {
   let wrapper;
+  let mockFetchCards;
+  let mockDisplayFavorites;
+  let mockFavorites;
+
 
   beforeEach(() => {
-    wrapper = shallow(<Nav />);
+    mockFetchCards = jest.fn();
+    mockDisplayFavorites = jest.fn();
+    mockFavorites = [
+      {
+        name: "Luke Skywalker",
+        class: "people-card",
+        data: {
+          homeworld: "Tatooine",
+          language: "Galactic Basic",
+          population: "200000",
+          species: "Human"
+        }
+      }
+    ];
+    wrapper = shallow(<Nav fetchCards={mockFetchCards}
+      favorites={mockFavorites}
+      displayFavorites={mockDisplayFavorites} />);
   });
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display the length of favorites array', () => {
+    expect(wrapper.find('span').text()).toEqual('1');
   });
 });
